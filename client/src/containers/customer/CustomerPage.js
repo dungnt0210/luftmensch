41140d3c1fcbc4de1c569b1e2376d0430b0a5855
dungntbss <dungnt@bsscommerce.com>
2021-06-03 17:10:58 +0700

    refact to tabs

--- client/src/containers/customer/CustomerPage.js
+++ client/src/containers/customer/CustomerPage.js
@@ -1,43 +1,32 @@
 import React, {useEffect} from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import { connect } from 'react-redux';
-import { Menu } from 'antd';
+import { Menu, Tabs } from 'antd';
 import Login from "../../components/customer/Login";
 import Account from "../../components/customer/Account";
 import "./customer.scss"
 const CustomerPage = ({isAuthenticated}) => {
-
+    const { TabPane } = Tabs;
     return (
-        <BrowserRouter >
-          <Menu mode="inline" defaultSelectedKeys={['customer-account']} className="customer-menu">
-            <Menu.Item key="customer-account">
-              <Link to={"/customer/account"}>My Account</Link>
-            </Menu.Item>
-            <Menu.Item key="customer-addresses">
-              <Link to={"/customer/addresses"}>My Addresses</Link>
-            </Menu.Item>
-            <Menu.Item key="customer-orders">
-              <Link to={"/customer/orders"}>My Orders</Link>
-            </Menu.Item>
-            <Menu.Item key="customer-wishlist">
-              <Link to={"/customer/wishlist"}>My Wishlist</Link>
-            </Menu.Item>
-            <Menu.Item key="customer-reviews">
-              <Link to={"/customer/reviews"}>My Reviews</Link>
-            </Menu.Item>
-          </Menu>
-          <div className="customer-content">
-            <Switch>
-                <Route path="/customer/orders/:id" component={Login} />
-                <Route path="/customer/orders" component={Login} />
-                <Route path="/customer/addresses/:id" component={Login} />
-                <Route path="/customer/addresses" component={Login} />
-                <Route path="/customer/reviews" component={Login} />
-                <Route path="/customer/wishlist" component={Login} />
-                <Route path="/" component={Account} />
-            </Switch>
-          </div>
-         </BrowserRouter>
+        <div className="customer-content">
+              <Tabs tabPosition="left">
+              <TabPane tab="My Account" key="customer-account">
+                <Account />
+              </TabPane>
+              <TabPane tab="My Addresses" key="customer-addresses">
+                Content of Tab 2
+              </TabPane>
+              <TabPane tab="My Orders" key="customer-orders">
+                Content of Tab 3
+              </TabPane>
+              <TabPane tab="My Saved Items" key="customer-wishlist">
+                Content of Tab 4
+              </TabPane>
+              <TabPane tab="My Reviews" key="customer-reviews">
+                Content of Tab 5
+              </TabPane>
+            </Tabs>
+        </div>
     );
  };
  const mapStateToProps = state => ({
