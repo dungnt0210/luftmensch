05aa057547d504ebe0d981ca62ca599110eee6ed
dungntbss <dungnt@bsscommerce.com>
2021-06-15 17:19:05 +0700

    tt

--- client/src/components/customer/Addresses.js
+++ client/src/components/customer/Addresses.js
@@ -7,6 +7,14 @@ import Address from "./Address";
 const Addresses = ({defaultAddress, addressList}) => {
      const [isEditing, setIsEditing] = useState(false);
      const [checkedAddress, setCheckedAddress] = useState({isEmpty: true});
+     const [hasAddress, setHasAddress] = useState(false);
+     useEffect( () => {
+      if (typeof defaultAddress === 'undefined') {
+        setHasAddress(false);
+      } else {
+        setHasAddress(true);
+      }
+     }, [defaultAddress, addressList])
      const onCancel = () => {
        setIsEditing(false);
      }
@@ -14,8 +22,10 @@ const Addresses = ({defaultAddress, addressList}) => {
       setIsEditing(false);
     }
     const newAddress = () => {
-      setIsEditing(true);
-      setCheckedAddress({isEmpty: true});
+      console.log(hasAddress);
+      console.log(defaultAddress);
+      // setIsEditing(true);
+      // setCheckedAddress({isEmpty: true});
     }
     const editAddress = (item) => {
       setIsEditing(true);
@@ -34,7 +44,49 @@ const Addresses = ({defaultAddress, addressList}) => {
                 />
         </Row>
         <Divider />
-        <Row gutter={16}>
+        {
+          hasAddress ?
+          ( <>        
+            <Row gutter={16}>
+              <Col span={12}>
+              <Card title="Default address" className="default-address" extra={[<Button onClick={e => editAddress(defaultAddress)} icon={<EditOutlined />}  key="edit-address" >Edit</Button>]}>
+              <p>Phone number: {defaultAddress.telephone}</p>
+              {defaultAddress.detail}, {defaultAddress.commune.name}, {defaultAddress.district.name}, {defaultAddress.province.name}
+              </Card>
+              </Col>
+            </Row>
+            <Divider />
+            <Row>
+                {
+                  addressList.length ?
+                  (<Typography.Title level={3}>More addresses</Typography.Title>) : null}
+                <Col span={24}>
+                    <List
+                    grid={{
+                    gutter: 16,
+                    column: 2
+                    }}
+                    dataSource={addressList}
+                    renderItem={item => (
+                    <List.Item>
+                        <Card title={item.telephone} actions={
+                          [
+                            <Button icon={<CheckOutlined />} type="primary" key={`set-default-address-${item.index}`}>Set as default</Button>,
+                            <Button icon={<EditOutlined />} onClick={e => editAddress(item)} key={`edit-address-${item.index}`} >Edit</Button>,
+                            <Button icon={<DeleteOutlined />} type="danger" key={`delete-address-${item.index}`}>Remove</Button>,
+                          ]
+                        }>
+                        {item.detail}, {item.commune.name}, {item.district.name}, {item.province.name}
+                        </Card>
+                    </List.Item>
+                    )}
+                />
+                </Col>
+            </Row>
+            </>
+        ) : <Typography.Title level={3}>You don't have any addresses</Typography.Title>
+        }
+        {/* <Row gutter={16}>
             <Col span={12}>
                 <Card title="Default address" className="default-address" extra={[<Button onClick={e => editAddress(defaultAddress)} icon={<EditOutlined />}  key="edit-address" >Edit</Button>]}>
                 <p>Phone number: {defaultAddress.telephone}</p>
@@ -67,7 +119,7 @@ const Addresses = ({defaultAddress, addressList}) => {
                 )}
             />
             </Col>
-        </Row>
+        </Row> */}
         </>
     );
 }
